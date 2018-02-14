452ddd869e1c84ed93d2af7f3b85c19aa34dcdba
Evan Wilt <ev.wilt@gmail.com>
2018-02-14 10:08:15 -0500

    Added drawn rectangles for board

--- board.lua
+++ board.lua
@@ -2,11 +2,17 @@ local board = {
     matrix = {}
 }
 
-function board.initMatrix()
-    for x = 1, 10 do
+function board.initMatrix(minX, maxX, minY, maxY)
+    minX = minX or 1
+    maxX = maxX or 10
+    minY = minY or 1
+    maxY = maxY or 20
+    for x = minX, maxX do
         board.matrix[x] = {}
-        for y = 1, 20 do
+        for y = minY, maxY do
             board.matrix[x][y] = 0
+            love.graphics.setColor(196, 207, 161)
+            love.graphics.rectangle("fill", 50 + x * 25, 50 + y * 25, 25, 25)
         end
     end
 end
--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@
 
 function love.load()
     love.window.setMode(800, 600, {resizable=true, vsync=false, minwidth=400, minheight=300})
+    love.graphics.setBackgroundColor(65, 65, 65)
     board = require("board")
 end
 
