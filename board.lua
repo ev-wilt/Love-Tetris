f38199a4e1901fba6fa4a8ea3006e8d78a2f28e2
Evan Wilt <ev.wilt@gmail.com>
2018-02-14 21:48:33 -0500

    Created and inserted sprite for boarder

--- board.lua
+++ board.lua
@@ -1,14 +1,17 @@
 local board = {
     matrix = {},
-    minX = 1, maxX = 10,
+    minX = 1, maxX = 12,
     minY = 1, maxY = 20,
+    boarder = love.graphics.newImage("sprites/boarder.png")    
 }
 
 function board.initMatrix()
     for x = board.minX, board.maxX do
         board.matrix[x] = {}
         for y = board.minY, board.maxY do
-            board.matrix[x][y] = 0
+            if x ~= board.minX and x ~= board.maxX then
+                board.matrix[x][y] = 0
+            end
         end
     end
     return board.matrix
@@ -20,6 +23,8 @@ function board.drawMatrix()
             if board.matrix[x][y] == 0 then
                 love.graphics.setColor(196, 207, 161)
                 love.graphics.rectangle("fill", 50 + x * 25, 50 + y * 25, 25, 25)
+            elseif board.matrix[x][y] == nil then
+                love.graphics.draw(board.boarder, 50 + x * 25, 50 + y * 25)
             end
         end
     end
