e95d5195a4ad335ee7edc478ba8992e49dabfd00
Evan Wilt <ev.wilt@gmail.com>
2018-03-02 10:31:23 -0500

    Made it so piece doesn't shift when player is moving it down, allow for press and holding buttons

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@
 function love.load()
     love.window.setMode(800, 600, {resizable=true, vsync=false, minwidth=400, minheight=300})
     love.graphics.setBackgroundColor(65, 65, 65)
+    love.keyboard.setKeyRepeat(true)
     math.randomseed(os.time())
     board = require("board")
     dropTimer = 0
@@ -15,9 +16,9 @@ end
 
 function love.update(dt)
     dropTimer = dropTimer + dt * speedMultiplier
-    if dropTimer > 1 then
+    if dropTimer >= 1 and love.keyboard.isDown("s") == false then
         board:shiftPiece(0, 1)
-        dropTimer = dropTimer - 1
+        dropTimer = 0
     end
 end
 
